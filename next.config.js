617cf74199553bb38f4de49402e0b9d01b6f8c74
a0m0rajab <rajab-2@windowslive.com>
2025-11-23 06:11:56 +0300

    chore: add skew prevention

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    experimental: {
+        useDeploymentId: true,
+        // Optionally, use with Server Actions
+        useDeploymentIdServerActions: true,
+    },
+};
 
 module.exports = nextConfig
