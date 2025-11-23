1976f97b5589929c46a58c1854d955dbd3d3683f
a0m0rajab <rajab-2@windowslive.com>
2025-11-23 06:14:36 +0300

    chore: show images

--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    images: {
+        unoptimized: false,
+    },
     experimental: {
         useDeploymentId: true,
         // Optionally, use with Server Actions
